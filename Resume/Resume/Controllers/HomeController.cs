cd77f9b3c2788622b3f79c801ba370cbf1893e8f
Aygul Zeynalova <aygl.zeynalova@mail.ru>
2024-07-25 23:25:34 +0400

    first commit

---
+++ Resume/Resume/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Models.Contexts;
+
+namespace Resume.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DataContext db;
+
+        public HomeController(DataContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Resume/Resume/Models/Contexts/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Resume.Models.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+    }
+}
